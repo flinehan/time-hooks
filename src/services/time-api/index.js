ec80f807a8e22c9c9ef1d44b73a068969c3aec84
flinehan <frankie.linehan@gmail.com>
2020-09-27 12:20:23 -0700

    adding time api and tests. Api is just using localstorage to share data.

---
+++ src/services/time-api/index.js
@@ -0,0 +1,50 @@
+/**
+ * This would be a library to fetch and work with our time "api"
+ * just using local storage as our api "db" this is a nice way to quickly prototype if there is no api
+ */
+export default class TimeApi {
+  constructor(storage = localStorage, namespace = 'v1/time') {
+    this.storage = storage
+    this.timeApiNameSpace = namespace
+  }
+
+  /**
+   * mock network activity
+   * @param {*} callback 
+   */
+  _simulateApiCall(callback) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(callback())
+      }, 300)
+    })
+  }
+
+  /**
+   * get all data under the namespace
+   */
+  get() {
+    return this._simulateApiCall(() => {
+      const time = localStorage.getItem(this.timeApiNameSpace)
+
+      if (time) {
+        return JSON.parse(time)
+      }
+
+      return {}
+    })
+  }
+
+  /**
+   * update data
+   * @param {*} value 
+   */
+  update(value) {
+    return this._simulateApiCall(() => {
+      localStorage.setItem(this.timeApiNameSpace, JSON.stringify(value))
+      return JSON.parse(localStorage.getItem(this.timeApiNameSpace)) 
+    })
+  }
+}
+
+export const timeApi = new TimeApi()
---
+++ src/services/time-api/time-api.test.js
@@ -0,0 +1,23 @@
+import { timeApi } from './index';
+
+describe('timeApi', () => {
+  test('should get data', (done) => {
+    timeApi.get()
+      .then((data) => {
+        expect(data).toEqual({});
+        done()
+      })
+  });
+
+  test('should update data', (done) => {
+    const test = {foo: 123}
+
+    timeApi.update(test)
+      .then((data) => {
+        expect(data).toEqual(test);
+        done()
+      })
+  });
+})
+
+
